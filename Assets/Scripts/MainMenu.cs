6b5f5955991ccafac4b587a74773630bf3a1d773
yangyang guo <94036888+IceMiaoMiao@users.noreply.github.com>
2022-09-10 00:10:18 +0800

    制作了马的骨骼动画

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.U2D.Animation;
 
 public class MainMenu : MonoBehaviour
 {
@@ -8,6 +9,10 @@ public class MainMenu : MonoBehaviour
     //public Material fishMat;
     
     public RuntimeAnimatorController fishAnim;
+
+    public SpriteSkin boneAnim;
+    public RuntimeAnimatorController horseAnim;
+
     public void PickImage(  )
     {
         NativeGallery.Permission permission = NativeGallery.GetImageFromGallery( ( path ) =>
@@ -16,7 +21,7 @@ public class MainMenu : MonoBehaviour
             if( path != null )
             {
                 // Create Texture from selected image
-                Texture2D texture = NativeGallery.LoadImageAtPath( path, 1024 );
+                Texture2D texture = NativeGallery.LoadImageAtPath( path, 4096 );
 
                 //Fish.GetComponent<SpriteRenderer>().material.mainTexture = texture;
                 
@@ -30,7 +35,7 @@ public class MainMenu : MonoBehaviour
                 GameObject quad = GameObject.CreatePrimitive( PrimitiveType.Quad );
                 quad.transform.position = Camera.main.transform.position + Camera.main.transform.forward * 2.5f;
                 quad.transform.forward = Camera.main.transform.forward;
-                quad.transform.localScale = new Vector3( 1f, texture.height / (float) texture.width, 1f );
+                quad.transform.localScale = new Vector3( 5f, texture.height / (float) texture.width, 5f );
     
                 Material material = quad.GetComponent<Renderer>().material;
                 /*
@@ -56,4 +61,58 @@ public class MainMenu : MonoBehaviour
     
         Debug.Log( "Permission result: " + permission );
     }
+    public void AddBoneAnimation( )
+        {
+            NativeGallery.Permission permission = NativeGallery.GetImageFromGallery( ( path ) =>
+            {
+                //Debug.Log( "Image path: " + path );
+                if( path != null )
+                {
+                    // Create Texture from selected image
+                    Texture2D texture = NativeGallery.LoadImageAtPath( path, 4096 );
+    
+                    //Fish.GetComponent<SpriteRenderer>().material.mainTexture = texture;
+                    
+                    if( texture == null )
+                    {
+                        Debug.Log( "Couldn't load texture from " + path );
+                        return;
+                    }
+        
+                    // Assign texture to a temporary quad and destroy it after 5 seconds
+                    GameObject quad = new GameObject("quad");
+                    SpriteRenderer sRender=quad.AddComponent<SpriteRenderer>();
+                    quad.transform.position = Camera.main.transform.position + Camera.main.transform.forward * 2.5f;
+                    quad.transform.forward = Camera.main.transform.forward;
+                    quad.transform.localScale = new Vector3( 6f, 6f, 6f );
+        
+                    Material material = quad.GetComponent<Renderer>().material;
+                    /*
+                    if( !material.shader.isSupported ) // happens when Standard shader is not included in the build
+                        material.shader = Shader.Find( "Universal Render Pipeline/2D/Sprite-Unlit-Default" );
+                    */
+                    
+                    material.shader = Shader.Find( "Universal Render Pipeline/2D/Sprite-Unlit-Default" );
+                    
+                    material.mainTexture = texture;
+                    Rect rect = new Rect(0, 0, texture.width, texture.height);
+                    Vector2 pivot = new Vector2(0.5f, 0.5f);
+                    sRender.sprite = Sprite.Create(texture, rect,pivot);
+                    
+                    Animator anim = quad.AddComponent<Animator>();
+                    anim.runtimeAnimatorController = horseAnim;
+
+                    SpriteSkin spriteSkin = quad.AddComponent<SpriteSkin>();
+                    spriteSkin = boneAnim;
+                    
+                    
+                    Destroy( quad, 80f );
+        
+                    // If a procedural texture is not destroyed manually, 
+                    // it will only be freed after a scene change
+                    Destroy( texture, 80f );
+                }
+            } );
+            
+        }
 }
